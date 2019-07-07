1fa3e74a3d3c049d1cfbfc5da68b1aca6fd854f9
Ravindar Gujral <ravindar@Ravindars-MBP.fios-router.home>
2019-07-06 22:04:22 -0400

    used moment and numeral to format dates and curreny

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
@@ -9,7 +11,8 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       </Link>
     </h3>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format("$0,0.00")}-
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
