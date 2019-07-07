4a692497ceef67826f4f899dbb18e723c1bdc34c
Ravindar Gujral <ravindar@Ravindars-MBP.fios-router.home>
2019-07-07 12:39:39 -0400

    edit/remove ability via firebase

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { editExpense, removeExpense } from "../actions/expenses";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 import ExpenseForm from "./ExpenseForm";
 
 const EditExpensePage = props => {
@@ -9,13 +9,13 @@ const EditExpensePage = props => {
       <ExpenseForm
         expense={props.expense}
         onSubmit={expense => {
-          props.dispatch(editExpense(props.expense.id, expense));
+          props.dispatch(startEditExpense(props.expense.id, expense));
           props.history.push("/");
         }}
       />
       <button
         onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.dispatch(startRemoveExpense({ id: props.expense.id }));
           props.history.push("/");
         }}
       >
